a3b3b605a2f0220b6e3936035841ce38e343f43a
JDeWeese <jdeweese574@icloud.com>
2024-10-08 09:23:41 -0500

    Initial Commit

---
+++ AtomicityTests/AtomicityTests.swift
@@ -0,0 +1,16 @@
+//
+//  AtomicityTests.swift
+//  AtomicityTests
+//
+//  Created by Joseph DeWeese on 10/8/24.
+//
+
+import Testing
+
+struct AtomicityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
